828e2286204cd24854e2ec35221d36864f07f170
muriloabreu <murilo.lilo@hotmail.com>
2023-04-27 21:25:24 -0300

    Implementação classe ClienteModel, adição de atributos endereço

--- src/main/java/com/api/controleestoque/models/ClienteModel.java
+++ src/main/java/com/api/controleestoque/models/ClienteModel.java
@@ -24,6 +24,18 @@ public class ClienteModel {
 	private String cpf;
 	@Column(nullable = false,  length = 100)
 	private String telefone;
+	@Column(nullable = false, length = 15)
+	private String cep;
+	@Column(nullable = false, length = 100)
+	private String logradouro;
+	@Column(nullable = false, length = 200)
+	private String complemento;
+	@Column(nullable = false, length = 100)
+	private String bairro;
+	@Column(nullable = false, length = 100)
+	private String localidade;
+	@Column(nullable = false, length = 5)
+	private String uf;
 	
 	/*Construtor*/
 	
@@ -31,15 +43,25 @@ public class ClienteModel {
 		super();
 	}
 
-	public ClienteModel(Long id, String nome, String sobreNome, String cpf, String telefone) {
+	
+	public ClienteModel(Long id, String nome, String sobreNome, String cpf, String telefone, String cep,
+			String logradouro, String complemento, String bairro, String localidade, String uf) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.sobreNome = sobreNome;
 		this.cpf = cpf;
 		this.telefone = telefone;
+		this.cep = cep;
+		this.logradouro = logradouro;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.localidade = localidade;
+		this.uf = uf;
 	}
-	
+
+
+
 	/*Métodos Acessores*/
 
 	public Long getId() {
@@ -82,16 +104,80 @@ public class ClienteModel {
 		this.telefone = telefone;
 	}
 
+
+	public String getCep() {
+		return cep;
+	}
+
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+
+	public String getBairro() {
+		return bairro;
+	}
+
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+
+	public String getUf() {
+		return uf;
+	}
+
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+
 	@Override
 	public String toString() {
 		return "ClienteModel [id=" + id + ", nome=" + nome + ", sobreNome=" + sobreNome + ", cpf=" + cpf + ", telefone="
-				+ telefone + "]";
+				+ telefone + ", cep=" + cep + ", logradouro=" + logradouro + ", complemento=" + complemento
+				+ ", bairro=" + bairro + ", localidade=" + localidade + ", uf=" + uf + "]";
 	}
+
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(cpf, id, nome, sobreNome, telefone);
+		return Objects.hash(bairro, cep, complemento, cpf, id, localidade, logradouro, nome, sobreNome, telefone, uf);
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -102,9 +188,14 @@ public class ClienteModel {
 		if (getClass() != obj.getClass())
 			return false;
 		ClienteModel other = (ClienteModel) obj;
-		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id) && Objects.equals(nome, other.nome)
-				&& Objects.equals(sobreNome, other.sobreNome) && Objects.equals(telefone, other.telefone);
+		return Objects.equals(bairro, other.bairro) && Objects.equals(cep, other.cep)
+				&& Objects.equals(complemento, other.complemento) && Objects.equals(cpf, other.cpf)
+				&& Objects.equals(id, other.id) && Objects.equals(localidade, other.localidade)
+				&& Objects.equals(logradouro, other.logradouro) && Objects.equals(nome, other.nome)
+				&& Objects.equals(sobreNome, other.sobreNome) && Objects.equals(telefone, other.telefone)
+				&& Objects.equals(uf, other.uf);
 	}
+
 	
 	
 	
