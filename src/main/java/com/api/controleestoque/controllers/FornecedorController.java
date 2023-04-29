8a168aea744b7071fd71c6e35b08cc7475a7acd7
muriloabreu <murilo.lilo@hotmail.com>
2023-04-29 10:54:00 -0300

    Criação de endpoints DELETE, PUT, GetOneID  classe FornecedorControllers

--- src/main/java/com/api/controleestoque/controllers/FornecedorController.java
+++ src/main/java/com/api/controleestoque/controllers/FornecedorController.java
@@ -3,13 +3,17 @@ package com.api.controleestoque.controllers;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,9 +52,56 @@ public class FornecedorController {
 	}
 	
 	@GetMapping
-	public ResponseEntity<List<FornecedorModel>> findAllFornecedores(){
+	public ResponseEntity<List<FornecedorModel>> getAllFornecedores(){
 		
 		return ResponseEntity.status(HttpStatus.OK).body(fornecedorService.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getOneFornecedor(@PathVariable(value = "id") Long id){
+		
+		Optional<FornecedorModel> fornecedorOptional = fornecedorService.findById(id);
+		if (!fornecedorOptional.isPresent()) {
+			
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Fornecedor não encontrado. ");
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(fornecedorService.findById(id));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteFornecedor(@PathVariable(value = "id") Long id) {
+
+		Optional<FornecedorModel> fornecedorOptional = fornecedorService.findById(id);
+		if (!fornecedorOptional.isPresent()) {
+			
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Fornecedor não encontrado. ");
+		}
+
+		fornecedorService.delete(fornecedorOptional.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Fornecedor deletado com sucesso. ");
+
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updateConsumo(@PathVariable(value = "id") Long id,
+			                                        @RequestBody @Valid FornecedorDtos fornecedorDtos) {
+
+		Optional<FornecedorModel> fornecedorOptional = fornecedorService.findById(id);
+		if (!fornecedorOptional.isPresent()) {
+			
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Fornecedor não encontrado. ");
+		}
+		
+		var fornecedorModel = fornecedorOptional.get();
+		fornecedorModel.setNome(fornecedorDtos.getNome());
+		fornecedorModel.setCnpj(fornecedorDtos.getCnpj());
+		
+		
+				
+		return ResponseEntity.status(HttpStatus.OK).body(fornecedorService.save(fornecedorModel));
+	
+
 	}
 	
 	
