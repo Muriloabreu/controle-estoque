e0633175ef1274e22840c6d729e10fc34c4cf8ab
muriloabreu <murilo.lilo@hotmail.com>
2023-04-28 21:42:05 -0300

    IMplementação da classe FornecedorModel e criação da interface FornecedorRepository

--- src/main/java/com/api/controleestoque/models/FornecedorModel.java
+++ src/main/java/com/api/controleestoque/models/FornecedorModel.java
@@ -16,5 +16,46 @@ public class FornecedorModel {
 	private Long id;
 	@Column(nullable = false, length = 100)
 	private String nome;
+	@Column(nullable = false, length = 100)
+	private String cnpj;
+	
+	/* Construtor */
+	public FornecedorModel() {
+		super();
+	}
+
+	public FornecedorModel(Long id, String nome, String cnpj) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cnpj = cnpj;
+	}
+
+	/* Métodos Acessores */
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	
 
 }
---
+++ src/main/java/com/api/controleestoque/repositories/FornecedorRepository.java
@@ -0,0 +1,10 @@
+package com.api.controleestoque.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.controleestoque.models.FornecedorModel;
+
+public interface FornecedorRepository extends JpaRepository<FornecedorModel, Long> {
+
+	boolean existsByNome(String nome);
+}
