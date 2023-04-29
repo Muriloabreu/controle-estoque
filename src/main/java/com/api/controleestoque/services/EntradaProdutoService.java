e5d0ff74a613777f888396c7004c93b27c928e66
muriloabreu <murilo.lilo@hotmail.com>
2023-04-29 09:31:46 -0300

    Alteração de nome Interface ClienteService para EntradaProdutoService

--- src/main/java/com/api/controleestoque/services/EntradaProdutoService.java
+++ src/main/java/com/api/controleestoque/services/EntradaProdutoService.java
@@ -3,17 +3,17 @@ package com.api.controleestoque.services;
 import java.util.List;
 import java.util.Optional;
 
-import com.api.controleestoque.models.ClienteModel;
+import com.api.controleestoque.models.EntradaProdutoModel;
 
 
 
 
-public interface ClienteService {
+public interface EntradaProdutoService {
 	
-	List<ClienteModel> findAll();
-	Optional<ClienteModel> findById(Long id);
-	ClienteModel save(ClienteModel cliente);
-	void delete(ClienteModel livro);
-	boolean existsByCpf(String cpf);
+	List<EntradaProdutoModel> findAll();
+	Optional<EntradaProdutoModel> findById(Long id);
+	EntradaProdutoModel save(EntradaProdutoModel entProduto);
+	void delete(EntradaProdutoModel entProduto);
+	
 
 }
