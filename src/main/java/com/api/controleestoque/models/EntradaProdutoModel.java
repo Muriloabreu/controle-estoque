f63fd2c7398a17108a7c9ae5a8377a7a28e8da77
muriloabreu <murilo.lilo@hotmail.com>
2023-04-29 10:02:05 -0300

    Implementações da classe EntradaProdutoModel, atributos

--- src/main/java/com/api/controleestoque/models/EntradaProdutoModel.java
+++ src/main/java/com/api/controleestoque/models/EntradaProdutoModel.java
@@ -1,12 +1,15 @@
 package com.api.controleestoque.models;
 
-import java.util.Objects;
 
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,8 +19,15 @@ public class EntradaProdutoModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
-	
+	@ManyToOne
+	@JoinColumn(name = "id_entrada_Produto")
+	private ProdutosModel produtosModel;
+	@Column(nullable = false)
+    private Double precoUnitario;
+    @Column(nullable = false)
+    private Integer quantidade;
+    @Column(nullable = false)
+    private LocalDateTime dataRegistro;
 	
 	
 	/*Construtor*/
@@ -25,11 +35,16 @@ public class EntradaProdutoModel {
 	public EntradaProdutoModel() {
 		super();
 	}
-
 	
-	
-	
-
+	public EntradaProdutoModel(Long id, ProdutosModel produtosModel, Double precoUnitario, Integer quantidade,
+			LocalDateTime dataRegistro) {
+		super();
+		this.id = id;
+		this.produtosModel = produtosModel;
+		this.precoUnitario = precoUnitario;
+		this.quantidade = quantidade;
+		this.dataRegistro = dataRegistro;
+	}
 
 
 	/*Métodos Acessores*/
@@ -42,6 +57,39 @@ public class EntradaProdutoModel {
 		this.id = id;
 	}
 
+	public ProdutosModel getProdutosModel() {
+		return produtosModel;
+	}
+
+	public void setProdutosModel(ProdutosModel produtosModel) {
+		this.produtosModel = produtosModel;
+	}
+
+	public Double getPrecoUnitario() {
+		return precoUnitario;
+	}
+
+	public void setPrecoUnitario(Double precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public LocalDateTime getDataRegistro() {
+		return dataRegistro;
+	}
+
+	public void setDataRegistro(LocalDateTime dataRegistro) {
+		this.dataRegistro = dataRegistro;
+	}
+	
+	
 	
 
 	
